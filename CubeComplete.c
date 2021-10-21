55f054be43dc7a46a5c0fc4f5e9e3ef15cf7ffa0
NakanoMiku13 <danulricht@hotmail.com>
2021-10-20 22:20:02 -0500

    Update

--- CubeComplete.c
+++ CubeComplete.c
@@ -2,6 +2,7 @@
 //Find more interesting codes in https://github.com/NakanoMiku13
 //DynamicTable
 #include<stdio.h>
+#include<time.h>
 #include <stdlib.h>
 //Defining the main structs
 typedef struct Table Table,*Function;
@@ -16,27 +17,35 @@ struct Table{
     Function (*Ctr)(Table *);
     Function (*Add)(Table *,int);
     Function (*Print)(Table *);
+    Function (*SumFace)(Table*,int);
 };
-Function Table_Ctr(Table *),Table_Add(Table *,int),Table_Print(Table *);
+Function Table_Ctr(Table *),Table_Add(Table *,int),Table_Print(Table *),Table_SumFace(Table*,int);
 int main(){
     struct Table table;
     table.Ctr=Table_Ctr;
     table.Add=Table_Add;
     table.Print=Table_Print;
+    table.SumFace=Table_SumFace;
     table.Ctr(&table);
-    int x,y,i,j,o=1,z,k;
-    printf("Rows and Cols\n");
+    int x,y,i,j,o=1,z,k,op;
+    printf("Type the Y coordinate, X coordinate and Z coordinate\n");
     scanf("%d %d %d",&y,&x,&z);
+    printf("Type the face that you want to make the sum operation\n1) Front Face\t2) Back Face\t3) Right Face\t4) Left Face\t5) Top Face\t6) Bottom Face\n");
+    scanf("%d",&op);
+    system("clear");
     table._cols=x;
     table._rows=y;
     table._back=z;
+    srand(time(NULL));
     for(i=0;i<y;i++){
         for(j=0;j<x;j++){
             for(k=0;k<z;k++,o++){
-                table.Add(&table,o);
+                table.Add(&table,rand()%100);
             }
         }
     }
+    table.SumFace(&table,op);
+    printf("This is the table:\n\n");
     table.Print(&table);
 }
 Function Table_Ctr(Table *Self){
@@ -46,7 +55,6 @@ Function Table_Ctr(Table *Self){
 Function Table_Add(Table *Self,int value) {
     struct Node* tmp;
     tmp=(struct Node*)malloc(sizeof(struct Node));
-    printf("\nValue: %d\n",value);
     tmp->Value=value;
     if(Self->_size>0){
         if(Self->_ccols<Self->_cols && Self->_crows==0 && Self->_cback==0){
@@ -81,8 +89,7 @@ Function Table_Add(Table *Self,int value) {
                 for(i=0;i<Self->_crows-1;i++,_MoveUp=_MoveUp->Bottom);
                 for(i=0;i<Self->_ccols;i++,_MoveUp=_MoveUp->Next);
             }
-            printf("%d\n",test->Back->Value);
-            for(i=0;i<Self->_cback;i++,_MoveRight=_MoveRight->Back){printf("v= %d\n",_MoveRight->Value);};
+            for(i=0;i<Self->_cback;i++,_MoveRight=_MoveRight->Back);
             for(i=0;i<Self->_ccols-1;i++,_MoveRight=_MoveRight->Next);
             if(Self->_crows>0) for(i=0;i<Self->_crows;i++,_MoveRight=_MoveRight->Bottom);
             tmp->Prev = _MoveRight;
@@ -126,7 +133,6 @@ Function Table_Add(Table *Self,int value) {
             int i=0;
             for(i=0;i<Self->_cback;i++,_Move=_Move->Back);
             tmp->Front=_Move;
-            printf("tt %d\n",tmp->Front->Value);
             _Move->Back=tmp;
             Self->_ccols=1;
             Self->_crows=0;
@@ -138,7 +144,6 @@ Function Table_Add(Table *Self,int value) {
             Self->_ccols=1;
             Self->_crows=0;
             Self->_cback++;
-            printf("Back\n");
         }
     }else{
         Self->Head=tmp;
@@ -160,5 +165,112 @@ Function Table_Print(Table *Self){
             printf("\n");
         }
         printf("\n");
+    }
+}
+int FrontFace(Table* t){
+    struct Node* Move = t->Head,*Move2=t->Head;
+    int i,j,r=0;
+    for(i=0;i<t->_rows;i++,Move2=Move2->Bottom){
+        Move=Move2;
+        for(j=0;j<t->_cols;j++,Move=Move->Next){
+            printf("%d ",Move->Value);
+            r+=Move->Value;
+        }
+        printf("\n");
+    }
+    return r;
+}
+int BackFace(Table *t){
+    struct Node* Move = t->Head,*Move2=t->Head;
+    int i,j,r=0;
+    for(i=0;i<t->_back-1;i++,Move=Move->Back,Move2=Move2->Back);
+    for(i=0;i<t->_rows;i++,Move2=Move2->Bottom){
+        Move=Move2;
+        for(j=0;j<t->_cols;j++,Move=Move->Next){
+            printf("%d ",Move->Value);
+            r+=Move->Value;
+        }
+        printf("\n");
+    }
+    return r;
+}
+int RightFace(Table * t){
+    struct Node * Move = t->Head,*Move2=t->Head;
+    int i,j,r=0;
+    for(i=0;i<t->_cols-1;i++,Move=Move->Next,Move2=Move2->Next);
+    for(i=0;i<t->_rows;i++,Move2=Move2->Bottom){
+        Move=Move2;
+        for(j=0;j<t->_back;j++,Move=Move->Back){
+            printf("%d ",Move->Value);
+            r+=Move->Value;
+        }
+        printf("\n");
+    }
+    return r;
+}
+int LeftFace(Table *t){
+    struct Node* Move = t->Head,*Move2=t->Head;
+    int i,j,r=0;
+    for(i=0;i<t->_rows;i++,Move2=Move2->Bottom){
+        Move=Move2;
+        for(j=0;j<t->_back;j++,Move=Move->Back){
+            printf("%d ",Move->Value);
+            r+=Move->Value;
+        }
+        printf("\n");
+    }
+    return r;
+}
+int TopFace(Table *t){
+    struct Node * Move = t->Head,*Move2=t->Head;
+    int i,j,r=0;
+    for(i=0;i<t->_back;i++,Move2=Move2->Back){
+        Move=Move2;
+        for(j=0;j<t->_cols;j++,Move=Move->Next){
+            printf("%d ",Move->Value);
+            r+=Move->Value;
+        }
+        printf("\n");
+    }
+    return r;
+}
+int BottomFace(Table *t){
+    struct Node * Move = t->Head,*Move2=t->Head;
+    int i,j,r=0;
+    for(i=0;i<t->_rows-1;i++,Move=Move->Bottom,Move2=Move2->Bottom);
+    for(i=0;i<t->_back;i++,Move2=Move2->Back){
+        Move=Move2;
+        for(j=0;j<t->_cols;j++,Move=Move->Next){
+            printf("%d ",Move->Value);
+            r+=Move->Value;
+        }
+        printf("\n");
+    }
+    return r;
+}
+Function Table_SumFace(Table *Self,int FaceNumber){
+    switch(FaceNumber){
+        case 1:
+            printf("Value: %d\n",FrontFace(Self));
+            break;
+        case 2:
+            printf("Value: %d\n",BackFace(Self));
+            break;
+        case 3:
+            printf("Value: %d\n",RightFace(Self));
+            break;
+            break;
+        case 4:
+            printf("Value: %d\n",LeftFace(Self));
+            break;
+        case 5:
+            printf("Value: %d\n",TopFace(Self));
+            break;
+        case 6:
+            printf("Value: %d\n",BottomFace(Self));
+            break;
+        default:
+            printf("Error\n");
+            break;
     }
 }
