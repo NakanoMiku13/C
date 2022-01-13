120a5dde61f461f6245139f834cb6e860a0be095
NakanoMiku13 <danulricht@hotmail.com>
2022-01-12 21:59:57 -0600

    Update to Moleculas

--- Moleculas.c
+++ Moleculas.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct Nodo Nodo;
-typedef struct grafo grafo,*funcionGrafo;
+typedef struct grafo grafo;
 typedef struct NodoLista NodoLista;
-typedef struct lista lista,*funcionLista;
+typedef struct lista lista;
 struct NodoLista{
     grafo* otroNucleo;
     NodoLista* siguiente;
@@ -15,30 +15,29 @@ struct Nodo{
 struct lista{
     NodoLista* cabeza;
     int longitud;
-    funcionLista (*crearLista)(lista*),(*agregarElemento)(lista*,grafo*),(*eliminarElemento)(lista*);
 };
 struct grafo{
     Nodo* nucleo;
     lista* otrosAtomos;
     int longitud;
-    funcionGrafo (*crearMolecula)(grafo*),(*unirAtomo)(grafo*,grafo*),(*eliminarAtomo)(grafo*),(*destruirAtomo)(grafo*);
 };
-funcionLista crearLista(lista*),agregarElemento(lista*,grafo*),eliminarElemento(lista*);
-funcionGrafo crearMolecula(grafo*),unirAtomo(grafo*,grafo*),eliminarAtomo(grafo*),destruirAtomo(grafo*);
+void crearLista(lista*),agregarElemento(lista*,grafo*),eliminarElemento(lista*);
+void crearMolecula(grafo*),unirAtomo(grafo*,grafo*),eliminarAtomo(grafo*),destruirAtomo(grafo*);
 void CrearMoleculas(grafo*);
 int main(){
     grafo *molecula,*molecula2;
     printf("Hola\n");
-    CrearMoleculas(molecula);
-    CrearMoleculas(molecula2);
-    molecula->unirAtomo(molecula,molecula2);
+    crearMolecula(molecula);
+    crearMolecula(molecula2);
+    unirAtomo(molecula,molecula2);
     printf("tam: %d\n",molecula->longitud);
+    scanf("%d");
 }
-funcionLista crearLista(lista* ls){
+void crearLista(lista* ls){
     ls->cabeza = (NodoLista*)malloc(sizeof(NodoLista));
     ls->longitud=0;
 }
-funcionLista agregarElemento(lista* ls,grafo* nodoAjeno){
+void agregarElemento(lista* ls,grafo* nodoAjeno){
     NodoLista* nuevoElemento = (NodoLista*)malloc(sizeof(NodoLista));
     nuevoElemento->otroNucleo=nodoAjeno;
     if(ls->longitud>0){
@@ -51,7 +50,7 @@ funcionLista agregarElemento(lista* ls,grafo* nodoAjeno){
     }
     ls->longitud++;
 }
-funcionLista eliminarElemento(lista* ls){
+void eliminarElemento(lista* ls){
     if(ls->longitud>0){
         NodoLista* aux = ls->cabeza;
         ls->cabeza=aux->siguiente;
@@ -59,41 +58,31 @@ funcionLista eliminarElemento(lista* ls){
         ls->longitud--;
     }
 }
-funcionGrafo crearMolecula(grafo* molecula){
+void crearMolecula(grafo* molecula){
     molecula->nucleo=(Nodo*)malloc(sizeof(Nodo));
     molecula->longitud=1;
-    molecula->otrosAtomos->agregarElemento = agregarElemento;
-    molecula->otrosAtomos->crearLista= crearLista;
-    molecula->otrosAtomos->eliminarElemento= eliminarElemento;
-    molecula->otrosAtomos->crearLista(molecula->otrosAtomos);
+    lista* list = molecula->otrosAtomos;
+    crearLista(list);
+    molecula->otrosAtomos=list;
 }
 //molecula es el nodo que vamos a unir a otra molecula
-funcionGrafo unirAtomo(grafo* moleculaOrigen,grafo* atomoAjeno){
+void unirAtomo(grafo* moleculaOrigen,grafo* atomoAjeno){
     printf("debug\n");
     atomoAjeno->nucleo->otroNucleo=moleculaOrigen->nucleo;
-    moleculaOrigen->otrosAtomos->agregarElemento(moleculaOrigen->otrosAtomos,atomoAjeno);
+    agregarElemento(moleculaOrigen->otrosAtomos,atomoAjeno);
     moleculaOrigen->longitud+=atomoAjeno->longitud;
 }
-funcionGrafo eliminarAtomo(grafo* molecula){
+void eliminarAtomo(grafo* molecula){
     if(molecula->longitud>1){
         if(molecula->otrosAtomos->longitud>0){
-            molecula->otrosAtomos->eliminarElemento(molecula->otrosAtomos);
+            eliminarElemento(molecula->otrosAtomos);
         }
     }
 }
-funcionGrafo destruirAtomo(grafo* molecula){
+void destruirAtomo(grafo* molecula){
     if(molecula->longitud>0 && molecula->longitud==1){
         if(molecula->otrosAtomos==0){
             free(molecula);
         }
     }
-}
-void CrearMoleculas(grafo* molecula){
-    printf("debug\n");
-    molecula->crearMolecula=crearMolecula;
-    molecula->unirAtomo=unirAtomo;
-    molecula->eliminarAtomo=eliminarAtomo;
-    molecula->destruirAtomo=destruirAtomo;
-    printf("debug\n");
-    molecula->crearMolecula(molecula);
 }
